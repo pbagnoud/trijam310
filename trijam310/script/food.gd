9eb5f2ad97c306aaf2834adf3256cc6f26068109
SolennEstier <solenn.estier@gmail.com>
2025-02-23 13:32:37 +0100

    le sang devient vieux

--- trijam310/script/food.gd
+++ trijam310/script/food.gd
@@ -1,10 +1,19 @@
 extends Area2D
 signal ate
 
-
+var old: bool = false
 
 
 func _on_body_entered(body: Node2D) -> void:
 	emit_signal('ate')
 	print('mangé')
 	queue_free()
+
+
+func _on_uptime_timeout() -> void:
+	if old :
+		print("disparu")
+		queue_free()
+	else :
+		$Sprite2D.frame = 1
+		old = true
