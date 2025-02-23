81f181b5c535c9e673a2f9c731a24ba0376c7705
charl-baum <charlottebaumgart61@gmail.com>
2025-02-23 13:49:50 +0100

    music

--- trijam310/script/main.gd
+++ trijam310/script/main.gd
@@ -1,15 +1,13 @@
 extends Node2D
 @onready var spawn_food_timer: Timer = $Spawn_food_timer
 @onready var pv: ProgressBar = $PV
-@onready var player: CharacterBody2D = $Player
-@onready var spawn_garlic_timer: Timer = $Spawn_garlic_timer
+
 @onready var spawn_area: ColorRect = $spawn_area
 var food_scene = preload("res://scene/food.tscn")
-var garlic_scene = preload("res://scene/ennemi.tscn")
 var rng = RandomNumberGenerator.new()
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Audio.play_music()
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,14 +23,3 @@ func _on_spawn_food_timer_timeout() -> void:
 	add_child(food)
 	food.ate.connect(pv._regen)
 	spawn_food_timer.start()
-
-
-func _on_spawn_garlic_timer_timeout() -> void:
-	var garlic = garlic_scene.instantiate()
-	var random_position = Vector2(
-		rng.randf_range(spawn_area.position.x, spawn_area.size.x),
-		rng.randf_range(spawn_area.position.y, spawn_area.size.y))
-	garlic.position = random_position
-	add_child(garlic)
-	garlic.hit.connect(player._on_hit)
-	spawn_garlic_timer.start()
--- trijam310/script/pv.gd
+++ trijam310/script/pv.gd
@@ -3,7 +3,6 @@ extends ProgressBar
 @onready var game_over: Label = $GameOver
 @onready var timer: Timer = $Timer
 @onready var color_rect: ColorRect = $ColorRect
-@onready var score_timer: Timer = $Score/ScoreTimer
  
 
 const DYING_SPEED = 15
@@ -12,27 +11,28 @@ var REGEN = self.max_value/2
 
 var current_score:float
 var in_game_over = 0
-var time_elapsed := 0.0
+var run_timer =0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.value= self.max_value
-	score_timer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	current_score = self.value
 	self.value= current_score - DYING_SPEED*delta
+	if in_game_over != 1:
+		run_timer = run_timer+delta
 
 	#Game Over
 	if self.value < 0.01 and in_game_over == 0:
 		color_rect.visible = 1
+		game_over.text="Score = "+str(int(run_timer))
 		game_over.visible = 1
 		in_game_over = 1
 		timer.start()
-		score_timer.paused = 1
-		game_over.text = "Score = " + str(score_timer)
-		
+
+	
 
 func _regen():
 	self.value = min(self.value+REGEN,self.max_value)
