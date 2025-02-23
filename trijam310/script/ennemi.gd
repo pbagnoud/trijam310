53588fb8f1d6cb0be70a0d41584e9fa399f60bc3
PB <119977776+pbagnoud@users.noreply.github.com>
2025-02-23 13:22:17 +0100

    enemi

--- trijam310/script/ennemi.gd
+++ trijam310/script/ennemi.gd
@@ -1,18 +1,23 @@
 extends Area2D
 
 signal hit
-
+var random_direction = Vector2()
+var speed = 15
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	random_direction = Vector2.from_angle(randf() * 2 * PI) # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	position += random_direction * delta
 
 
 func _on_body_entered(body: Node2D) -> void:
 	print("OUILLE")
 	hit.emit()
 	queue_free()
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free 
