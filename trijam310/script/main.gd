1a7536fd000bd6f06d9c8481f990d67af0116177
PB <119977776+pbagnoud@users.noreply.github.com>
2025-02-23 12:14:26 +0100

    Update main.gd

--- trijam310/script/main.gd
+++ trijam310/script/main.gd
@@ -1,5 +1,6 @@
 extends Node2D
 @onready var spawn_food_timer: Timer = $Spawn_food_timer
+@onready var pv: ProgressBar = $PV
 
 @onready var spawn_area: ColorRect = $spawn_area
 var food_scene = preload("res://scene/food.tscn")
@@ -20,4 +21,5 @@ func _on_spawn_food_timer_timeout() -> void:
 		rng.randf_range(spawn_area.position.y, spawn_area.size.y))
 	food.position = random_position
 	add_child(food)
+	food.ate.connect(pv._regen)
 	spawn_food_timer.start()
