40c07e23fe3122bcbae18f207b29f0e2724381cd
charl-baum <charlottebaumgart61@gmail.com>
2025-02-23 12:32:15 +0100

    GameOver sa mère

--- trijam310/script/pv.gd
+++ trijam310/script/pv.gd
@@ -2,7 +2,7 @@ extends ProgressBar
 
 @onready var game_over: Label = $GameOver
 @onready var timer: Timer = $Timer
-@onready var color_rect: ColorRect = $GameOver/ColorRect
+@onready var color_rect: ColorRect = $ColorRect
 
 
 const DYING_SPEED = 15
@@ -21,7 +21,7 @@ func _physics_process(delta: float) -> void:
 
 	#Game Over
 	if self.value < 0.01 and in_game_over == 0:
-		#color_rect.visible = 1
+		color_rect.visible = 1
 		game_over.visible = 1
 		in_game_over = 1
 		timer.start()
